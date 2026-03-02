8e51fc1eaf77933a2d14fbcdce53007e6d90a1c0
Mohammed Minuddin <minuddin7@gmail.com>
2026-03-02 14:17:03 +0600

    update database seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,12 +12,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // Base user
-        User::firstOrCreate(
-            ['email' => 'superadmin@example.com'],
-            ['name' => 'Super Admin', 'password' => bcrypt('password')] // Add a default password
-        );
-
         // Core seeders
         $this->call([
             FarmSeeder::class,
